ccec8db3f0f880cb3958523f290bb9ef1ec4762b
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2023-12-18 13:14:28 -0500

    Create test-long-fails.sh for rfc tests that are expected to fail typically because they require tables larger than the limits currently compiled into the rfc program code.

--- song-rfc/test-long-fails.sh
+++ song-rfc/test-long-fails.sh
@@ -5,15 +5,12 @@
 # TODO: This failed on file fw1_1K the last time I tried it, with a
 # segmentation fault.  Debug this to figure out what is going wrong.
 
-#CMD="./rfc"
-CMD="valgrind --tool=memcheck ./rfc"
+CMD="./rfc"
 
-set -e
 make
 for j in \
-    acl1_100 acl1_1K acl1_5K acl1_10K \
-    fw1_100 fw1_1K \
-    ipc1_100 ipc1_1K
+    fw1_5K fw1_10K \
+    ipc1_5K ipc1_10K
 do
     echo ""
     echo "-----------------------------------------------------------------"
@@ -21,8 +18,14 @@ do
     echo "-----------------------------------------------------------------"
     set -x
     ${CMD} -r ../orig/song-filterset/${j} -t ../orig/song-filterset/${j}_trace
+    exit_status=$?
+    if [ ${exit_status} -eq 0 ]
+    then
+	echo "Expected test ${j} to fail, but it succeeded."
+	exit 1
+    fi
     set +x
 done
 
 echo ""
-echo "All tests passed without errors."
+echo "All tests failed, as expected."
