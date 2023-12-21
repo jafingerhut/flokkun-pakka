0df5a9e4ab77a305c153d6e18124cb45e4247cc5
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2023-12-20 23:34:35 -0500

    Bumped up MAXNODES parameter to 4,000,000 for song-woo, so all but 2 tests pass

--- song-woo/test-long.sh
+++ song-woo/test-long.sh
@@ -2,21 +2,20 @@
 
 # A longer running set of tests of woo program.
 
-#CMD="./woo"
-CMD="valgrind --tool=memcheck ./woo"
+CMD="./woo"
+#CMD="valgrind --tool=memcheck ./woo"
 
 # tests with errors:
-# fw1_5K fw1_10K ipc1_5K
-#Fatal:list::operator&=: item 1000001 is outside of allowed range [1,1000000]
-# I tried increasing MAXNODES to 2000000, and they still failed.
-
-#    acl1_100 acl1_1K acl1_5K acl1_10K \
-#    fw1_100 fw1_1K \
+# fw1_5K fw1_10K
+#Fatal:list::operator&=: item 4000001 is outside of allowed range [1,4000000]
+# I tried increasing MAXNODES to 4000000, and they still failed.
 
 set -e
 make
 for j in \
-    ipc1_100 ipc1_1K ipc1_10K
+    acl1_100 acl1_1K acl1_5K acl1_10K \
+    fw1_100 fw1_1K \
+    ipc1_100 ipc1_1K ipc1_5K ipc1_10K
 do
     echo ""
     echo "-----------------------------------------------------------------"
