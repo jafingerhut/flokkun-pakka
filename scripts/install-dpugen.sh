11f8afb9834d35559ba86ecb14ab212c222c66cb
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2024-01-03 22:05:14 -0500

    Small improvement to install-dpugen.sh

--- scripts/install-dpugen.sh
+++ scripts/install-dpugen.sh
@@ -3,7 +3,7 @@
 # Only tested on Ubuntu 20.04 Linux so far
 
 sudo apt-get update
-sudo apt-get install python3-venv
+sudo apt-get install -y python3-venv
 cd $HOME
 python3 -m venv venv-dpugen
 source $HOME/venv-dpugen/bin/activate
